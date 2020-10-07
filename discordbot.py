02a9027f07b74f7498f9ea7590f8ae0f9bf966ef
niwa-08 <72508666+niwa-08@users.noreply.github.com>
2020-10-07 23:25:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
+import datetime
+import pytz
+
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,6 +15,11 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+
+@bot.command()
+async def test(ctx):
+    now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+    await ctx.send('Niwa Bot は正常に稼働中！\nTime:['+now+']')
 
 @bot.command()
 async def ping(ctx):
